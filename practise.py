b4700ea65515e102ba75011b84481c4be9771f0f
bharath2498 <mulagadabharath@gmail.com>
2026-09-17 00:08:38 +0530

    update practise.py

--- practise.py
+++ practise.py
@@ -88,45 +88,81 @@
 # dict_com = { employee["name"]:employee["salary"] for employee in employees }
 # print(dict_com)
 
-class Employee():
-    company = "infosys"
 
-    def __init__(self, name,salary,location):
+class Employee():
+    company = "infosys" #class variable
+    @classmethod #class methods
+    def change_company(cls, new_company):
+        cls.company = new_company
+
+    def __init__(self, name,salary,location):# insatance vaiables & init metods
         self.name = name
-        self.salary = salary
+        self.__salary = salary ## encapsulation uses to hide the sesitive date without calling from oustside the class
         self.location = location
 
-    def show_details(self):
-        print(self.name,self.location,self.salary)
+    def show_details(self):# method
+        print(self.name,self.location,self.__salary)
     def change_name(self,new_name):
         self.name = new_name
+    def get_salary(self):
+        print(self.__salary)
     def salary_increment(self,salary_increment):
-        self.salary = self.salary + salary_increment
+        self.__salary = self.__salary + salary_increment
     def location_change(self,new_loc):
         self.location = new_loc
-class devolper(Employee):
+    @staticmethod
+    def valid_salary(salary):
+        return salary > 0
+class devolper(Employee):# inheritance
     def __init__(self, name, salary, location,language):
-        super().__init__(name, salary, location)
+        super().__init__(name, salary, location)# method overriding
         self.language = language 
     def show_details(self):
         super().show_details()
         print(self.language)
 
-    
-dev1 = devolper(
-    "Bharath",
-    50000,
-    "hyd",
-    "Python"
-)
-dev1.show_details()
 
 
-# emp1 = Employee('Bharath',50000,'hyd')
+
+# Employee.change_company("Google")
+# print(Employee.company)
+
+# dev1 = devolper(
+#     "Bharath",
+#     50000,
+#     "hyd",
+#     "Python"
+# )
+# dev1.show_details()
+
+
+emp1 = Employee('Bharath',50000,'hyd')
+emp1.salary_increment(5000)
+emp1.get_salary()
+
+print(emp1.valid_salary(500000))
 # emp2 = Employee('Bharath',50000,'hyd')
-# print(dev1.company)
+# print(dev1.change_company("accenture"))
 # emp1.change_name("vijay")
 # emp1.salary_increment(25000)
 # emp1.location_change("banglore")
-# emp1.show_name()
+# emp1.show_details()
 # emp2.show_name()
+
+# from abc import ABC,abstractmethod
+# # class EmployeeRole(ABC):## abstraction
+# #     @abstractmethod
+# #     def work(self):
+# #         pass
+# # class Developer(EmployeeRole): ## polymorphism
+# #     def work(self):
+# #         print("Writing a code")
+
+# # class Tester(EmployeeRole):
+# #     def work(self):
+# #         print("testing a code")
+
+# # team_members = [Developer(),Tester()]
+# # for team_member in team_members:
+# #     team_member.work()
+# # emp = EmployeeRole()
